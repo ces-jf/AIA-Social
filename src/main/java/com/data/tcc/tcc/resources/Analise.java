46f3d1e7573a79c24f9339bbe59b8fd8a52ddfe8
LucasG96 <lucas.sist.inf@gmail.com>
2019-08-20 21:49:02 -0300

    Upload Project

---
+++ src/main/java/com/data/tcc/tcc/resources/Analise.java
@@ -0,0 +1,20 @@
+package com.data.tcc.tcc.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+@RestController
+@RequestMapping(value = "analises")
+public class Analise {
+
+	@GetMapping
+	public void analisar() throws Exception {
+		DataSource ds = new DataSource("src/resources/vendas.arff");
+		Instances ins = ds.getDataSet();		
+		String dados = ins.toString();		
+	}
+}
